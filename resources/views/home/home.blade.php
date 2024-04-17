b9407b053eb078549a2a9c0e20015b9f6fa710ee
Anjasmara Dwi Setiadi <enzoblackred@gmail.com>
2024-04-17 15:51:47 +0700

    add extension clock-work

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.main')
+@push('styles')
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+@endpush
 
-@section('viewer')
+@section('container')
 <div id="Category-Crud-Create">
     <h2>testing </h2>
 </div>
